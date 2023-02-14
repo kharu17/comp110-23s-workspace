2b3b0586b841da603a28752b3fda5372e30d8206
Katelyn Lien <klien@unc.edu>
2023-02-13 21:05:37 -0500

    using while loop

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,8 +1,10 @@
 """EX 02 - Gussing a secret word in one shot."""
-___author__ = "730459195"
+__author__ = "730459195"
 
 SECRET: str = "python"
 guess: str = str(input("What is your 6-letter guess? "))
+while (len(guess) != 6):
+    guess = str(input("That was not 6 letters! Try again: "))
 playing: bool = True
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -10,16 +12,21 @@ YELLOW_BOX: str = "\U0001F7E8"
 letter_idx: int = 0 
 boxes: str = ""
 
-while letter_idx < 6:
+while letter_idx < len(guess):
     if (guess[letter_idx] == SECRET[letter_idx]):
         boxes = boxes + GREEN_BOX
     else:
-        if guess[letter_idx] in SECRET:
+        index: int = 0
+        in_word: bool = False
+        while index < len(guess):
+            if (guess[letter_idx] == SECRET[index]):
+                in_word = True
+            index = index + 1
+        if (in_word):
             boxes = boxes + YELLOW_BOX
         else:
             boxes = boxes + WHITE_BOX
     letter_idx = letter_idx + 1
-
 
 while playing:
     if guess == SECRET:
@@ -27,14 +34,6 @@ while playing:
         print("Woo! You got it!")
         playing = False
     else:
-        if (len(guess) != 6):
-            guess = str(input("That was not 6 letters! Try again: "))
-        if (len(guess) == 6):
-            print(boxes)
-            print("Not quite. Play again soon!")
-            playing = False 
-
-
-       
-    
-
+        print(boxes)
+        print("Not quite. Play again soon!")
+        playing = False 
---
+++ lessons/fn_practice.py
@@ -0,0 +1,11 @@
+"""Example functions to learn definition and calling syntax"""
+
+def my_max(number1: int, number2: int) -> int:
+    """Returns max value out of 2 numbers"""
+    if number1 >= number2:
+        return number1
+    else:
+        return number2
+
+max_number: int = my_max(1,10)
+print(max_number)
